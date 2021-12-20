1ef898df0ec9bc14604f01c6b774d00623c24eac
Abhishek Tomar <Abhishek.Tomar@angelbroking.com>
2021-12-20 14:13:35 +0530

    fixed flag test

--- utils/flags/flags_test.go
+++ utils/flags/flags_test.go
@@ -8,14 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPort(t *testing.T) {
-	assert.Equal(t, constants.PortDefaultValue, flags.Port())
-}
-
-func TestEnv(t *testing.T) {
-	assert.Equal(t, constants.EnvDefaultValue, flags.Env())
-}
-
 func TestBaseConfigPath(t *testing.T) {
 	assert.Equal(t, constants.BaseConfigPathDefaultValue, flags.BaseConfigPath())
 }
