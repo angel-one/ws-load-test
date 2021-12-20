3d7b5eded46132dc6cd1ce51cdbddf319d1c9fb9
Abhishek Tomar <Abhishek.Tomar@angelbroking.com>
2021-12-21 00:01:14 +0530

    struct logic

---
+++ models/structure.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+)
+
+type Base struct {
+	URL, Proto string
+	Count      int
+	Msg        []byte
+	Delay      int
+	TickDelay  int
+	Path       string
+}
+
+type Routine struct {
+	SendTime    time.Time
+	ReceiveTime time.Time
+	Diff        time.Duration
+	ReceivedMsg string
+}
